391b6ba8fdbbb597bf5e122047bf49e8337ba39e
Shawn Beaton <43018242+Beat0154@users.noreply.github.com>
2020-01-26 21:35:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ function disableButton(){
     var email = document.getElementById("email");
     var message = document.getElementById("message");
     var submit = document.getElementById("submit");
-    if(name.value==""||email.value==""||message.value==""){
+    if(name.value==""||email.value==""||message.value=="")
+    {
         submit.ClassName = "disabled";
+        submit.disabled = "disabeld";
     }else{
         submit.className = "";
+        submit.disabled = "";
     }
 }
